2e5c7d6062049bae500c71a2a68d64793229f453
yx-storehouse <yx9296666@163.com>
2024-12-31 15:14:16 +0800

    Site updated: 2024-12-31 15:14:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/31/hello-world/"];function toRandomPost(){
+var posts=["2024/12/31/hello-world/","2024/12/31/Cursor/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
